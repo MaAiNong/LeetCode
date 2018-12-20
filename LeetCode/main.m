1494b64a31f140902bd6264035d640fb1445cb57
sunuo <xianyirenxunuo@gmail.com>
2018-12-20 23:27:43 +0800

    Initial Commit

---
+++ LeetCode/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LeetCode
+//
+//  Created by kevin on 2018/12/20.
+//  Copyright © 2018 徐诺. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
