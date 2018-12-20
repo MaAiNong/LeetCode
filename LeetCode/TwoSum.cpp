511b21f1b4009227a195312fe92337a536e7880b
sunuo <xianyirenxunuo@gmail.com>
2018-12-20 23:40:34 +0800

    TwoSum
    Signed-off-by: sunuo <xianyirenxunuo@gmail.com>

---
+++ LeetCode/TwoSum.cpp
@@ -0,0 +1,16 @@
+//
+//  TwoSum.cpp
+//  LeetCode
+//
+//  Created by kevin on 2018/12/20.
+//  Copyright © 2018 徐诺. All rights reserved.
+//
+
+#include "TwoSum.hpp"
+
+vector<int> Solution::twoSum(vector<int> &nums, int target)
+{
+    vector<int> result;
+    
+    return result;
+}
---
+++ LeetCode/TwoSum.hpp
@@ -0,0 +1,23 @@
+//
+//  TwoSum.hpp
+//  LeetCode
+//
+//  Created by kevin on 2018/12/20.
+//  Copyright © 2018 徐诺. All rights reserved.
+//
+
+#ifndef TwoSum_hpp
+#define TwoSum_hpp
+
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+    
+public:
+    vector<int> twoSum(vector<int>& nums, int target);
+    
+};
+
+#endif /* TwoSum_hpp */
